05a7bdde9cd5781405ec1ef9d829429bbc67e9f8
Anders Opstrup <anders@coffeecode.dk>
2018-04-24 10:59:35 +0200

    Added streetlights and crime

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-drawStreetLights = function (svg, projection) {
+drawCrime = function (svg, projection) {
   d3.json('data/Crime_Incidents_in_2017.geojson', function(json){
     svg.selectAll('circle')
       .data(json.features)
@@ -16,9 +16,27 @@ drawStreetLights = function (svg, projection) {
   })
 }
 
+drawStreetlights = function (svg, projection) {
+  d3.json('data/Street_Lights.geojson', function(json){
+    svg.selectAll('circle')
+      .data(json.features)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d) {
+        return projection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[0];
+      })
+      .attr('cy', function(d) {
+        return projection([d.geometry.coordinates[0], d.geometry.coordinates[1]])[1];
+      })
+      .attr('r', '3')
+      .style('fill', 'green')
+      .style('opacity', 0.75);
+  })
+}
+
 var map = washingtonMap()
   .mapSvg('#map')
   .geojson('data/dc.geojson')
-  .callbackList(drawStreetLights)
+  .callbackList(drawStreetlights)
 
 map()
