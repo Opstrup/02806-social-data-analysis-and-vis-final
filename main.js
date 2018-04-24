afc855feac1d44a8541b27b68c0e4b3f041fd5e7
Anders Opstrup <anders@coffeecode.dk>
2018-04-24 10:32:09 +0200

    Started on callback for streetlights

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+drawStreetLights = function (svg) {
+  console.log('The svg be like', svg)
+  
+  svg.append('circle')
+    .attr('cx', '100')
+    .attr('cy', '100')
+    .attr('r', '10')
+    .style('fill', 'red');
+}
+
 var map = washingtonMap()
   .mapSvg('#map')
   .geojson('data/dc.geojson')
-  .callbackList(function(){ console.log('Im a callback') })
+  .callbackList(drawStreetLights)
 
 map()
