51ad7bbc5cb8894d87ff8821054c64d5dcff1d7c
Anders Opstrup <anders@coffeecode.dk>
2018-05-07 20:12:16 +0200

    Changed topFive to create json obj of data instead

--- topFive/topFive.js
+++ topFive/topFive.js
@@ -1,6 +1,33 @@
 const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 
+/**
+ * Creates a file with the top five crimes in DC.
+ * File format
+ * {
+ *  Total: {
+ *    CrimeA: 100,
+ *    CrimeB: 200,
+ *    ....
+ *  },
+ *  Day: {
+ *    CrimeA: 100,
+ *    CrimeB: 200,
+ *    ....
+ *  },
+ *  Evening: {
+ *    CrimeA: 100,
+ *    CrimeB: 200,
+ *    ....
+ *  },
+ *  Midnight: {
+ *    CrimeA: 100,
+ *    CrimeB: 200,
+ *    ....
+ *  }
+ * }
+ */
+
 if (argv.o === undefined || argv.f === undefined) {
   throw 'Please select a input file and output file. Use -f for input and -o for output';
 }
@@ -14,6 +41,9 @@ fs.exists(dataFile, (exists) => {
   fs.readFile(dataFile, 'utf8', (err, data) => {
     if (err) throw err;
 
+    var res = { Total: {}, DAY: {}, EVENING: {}, MIDNIGHT: {} };
+    const SHIFTS = ['DAY', 'EVENING', 'MIDNIGHT'];
+
     obj = JSON.parse(data);
     counted = obj.features.reduce((acc, x) => {
         if (acc.get(x.properties['OFFENSE']) == undefined)
@@ -26,23 +56,17 @@ fs.exists(dataFile, (exists) => {
 
     countedTopFive = Array.from(counted)
       .sort((a, b) => { return b[1] - a[1] })
-      .slice(0, 5)
-      .reduce((acc, x) => {
-        var jsObj = {}
-        jsObj[x[0]] = x[1];
-        acc.push(jsObj)
-        return acc;
-      }, [])
+      .slice(0, 5);
 
-    topFive = obj.features.filter(x => {
-      return (x.properties['OFFENSE'] == 'THEFT/OTHER' ||
-        x.properties['OFFENSE'] == 'THEFT F/AUTO' ||
-        x.properties['OFFENSE'] == 'MOTOR VEHICLE THEFT' ||
-        x.properties['OFFENSE'] == 'ROBBERY' ||
-        x.properties['OFFENSE'] == 'ASSAULT W/DANGEROUS WEAPON')
-    })
+    countedTopFive.forEach((crime) => {
+      res.Total[crime[0]] = obj.features.filter(x => x.properties['OFFENSE'] == crime[0]).length
+      SHIFTS.forEach((shift) => {
+        res[shift][crime[0]] = obj.features.filter(x => x.properties['OFFENSE'] == crime[0])
+          .filter(x => x.properties['SHIFT'] == shift).length
+      })
+    });
 
-    let dataString = JSON.stringify(topFive);
+    let dataString = JSON.stringify(res);
     fs.writeFile(outputFile, dataString, 'utf8');
   })
 });
