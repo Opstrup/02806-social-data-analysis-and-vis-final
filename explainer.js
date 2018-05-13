c7c6ca7d8a520775b0033b1392d1b069d577c5fa
Anders Opstrup <anders@coffeecode.dk>
2018-05-13 15:14:04 +0200

    Added barchart for total crime

--- explainer.js
+++ explainer.js
@@ -97,17 +97,75 @@ init = () => {
   readDataFile('data/combined_crime_incidents.json').then((data) => {
     var result = sizeOfCrimes(data);
 
-    console.log(result);
-    // var countedCrimes = data.total.reduce((acc, x) => {
-    //   if (acc.get(x.properties[crimeConstants.crimeType.propName]) == undefined)
-    //       acc.set(x.properties[crimeConstants.crimeType.propName], 1)
-    //     else
-    //       acc.set(x.properties[crimeConstants.crimeType.propName], acc.get(x.properties[crimeConstants.crimeType.propName]) + 1)
-    //     return acc;
-    // }, new Map())
+    var totalCrime = [];
+    data.forEach((ds) => {
+      var countedCrimes = ds.reduce((acc, x) => {
+        if (acc.get(x.properties[crimeConstants.crimeType.propName]) == undefined)
+            acc.set(x.properties[crimeConstants.crimeType.propName], 1)
+          else
+            acc.set(x.properties[crimeConstants.crimeType.propName], acc.get(x.properties[crimeConstants.crimeType.propName]) + 1)
+          return acc;
+      }, new Map())
+      totalCrime.push(countedCrimes);
+    });
 
-    // console.log(countedCrimes);
-  
+    var crime = [
+      {
+        value: 2334 + 2362 + 2392 + 2464 + 2387 + 2270 + 1853,
+        label: "ASSAULT W/DANGEROUS WEAPON"
+      },
+      {
+        value: 126 + 89 + 104 + 105 + 160 + 136 + 115,
+        label: "HOMICIDE"
+      },
+      {
+        value: 9267 + 12099 + 12852 + 14592 + 14244 + 14517 + 14445,
+        label: "THEFT/OTHER"
+      },
+      {
+        value: 3762 + 2834 + 2665 + 3119 + 2906 + 2692 + 2406,
+        label: "MOTOR VEHICLE THEFT"
+      },
+      {
+        value: 3674 + 4202 + 3986 + 3277 + 3347 + 2972 + 2166,
+        label: "ROBBERY"
+      },
+      {
+        value: 8157 + 9767 + 10169 + 11332 + 11352 + 12138 + 10257,
+        label: "THEFT F/AUTO"
+      },
+      {
+        value: 4060 + 3616 + 3354 + 3178 + 2536 + 2124 + 1527,
+        label: "BURGLARY"
+      },
+      {
+        value: 47 + 34 + 35 + 26 + 18 + 6 + 5,
+        label: "ARSON"
+      },
+      {
+        value: 156 + 272 + 298 + 317 + 331 + 346 + 292,
+        label: "SEX ABUSE"
+      }
+    ];
+
+    var crimeBarChart = barChart()
+                          .barChartSvg('#crime-bar-chart')
+                          .setColorScheme(colorbrewer.YlGnBu[9])
+                          .height(530)
+                          .width(450)
+                          .onMouseOver((d) => {
+                            d3.select('#crime-bar-chart-tooltip')
+                              .classed('invis', false)
+                              .select('#desc')
+                              .text(d.label);
+                            d3.select('#crime-bar-chart-tooltip')
+                              .select('#value')
+                              .text(d.value);
+                          })
+                          .onMouseOut(() => d3.select('#crime-bar-chart-tooltip').classed('invis', true))
+                          .ds(crime);
+    crimeBarChart();
+
     populateUIElements('#crime-props', Object.keys(result.props).length);
     populateUIElements('#crime-2010', result['2010']);
     populateUIElements('#crime-2011', result['2011']);
