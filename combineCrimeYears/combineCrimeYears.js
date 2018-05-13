b27efc1dbdd292fb39263f07760003e05481ff53
Anders Opstrup <anders@coffeecode.dk>
2018-05-13 10:10:08 +0200

    Finished crime combiner module and started on explainer js

--- combineCrimeYears/combineCrimeYears.js
+++ combineCrimeYears/combineCrimeYears.js
@@ -1,47 +1,26 @@
 const fs = require('fs');
-const argv = require('minimist')(process.argv.slice(2));
+const outputFile = '../data/combined_crime_incidents.json';
+const crimeDatasets = ['Crime_Incidents_in_2010.geojson',
+                      'Crime_Incidents_in_2011.geojson',
+                      'Crime_Incidents_in_2012.geojson',
+                      'Crime_Incidents_in_2013.geojson',
+                      'Crime_Incidents_in_2014.geojson',
+                      'Crime_Incidents_in_2015.geojson',
+                      'Crime_Incidents_in_2016.geojson',
+                      'Crime_Incidents_in_2017.geojson']
 
-if (argv.o === undefined || argv.f === undefined) {
-  throw 'Please select a input file and output file. Use -f for input and -o for output';
-}
+var combinedCrimes = [];
+crimeDatasets.forEach((dataFile) => {
+  var data = fs.readFileSync('../data/' + dataFile, 'utf8')
 
-const dataFile = argv.f;
-const outputFile = argv.o;
+  var obj = JSON.parse(data);
+  var crimes = obj.features.reduce((acc, x) => {
+    acc.push(x);
+    return acc;
+  }, []);
 
-var obj;
-
-const getProps = (x) => {
-  if (argv._.length == 0) return {};
-
-  var props = {};
-  argv._.forEach((prop) => { 
-    props[prop] = x[prop];
-  })
-
-  return props;
-}
-
-fs.exists(dataFile, (exists) => {
-  if (!exists) throw 'The input file does not exists!';
-
-  fs.readFile(dataFile, 'utf8', (err, data) => {
-    if (err) throw err;
-  
-    obj = JSON.parse(data);
-    filteredFeatures = obj.features.reduce((acc, x) => {
-      let props = getProps(x.properties);
-      acc.push({ type: 'Feature',
-                 properties: props,
-                 geometry: x.geometry });
-      return acc;
-    }, []);
-  
-    let filteredObj = {
-      'type': 'FeatureCollection',
-      'features': filteredFeatures
-    }
-  
-    let dataString = JSON.stringify(filteredObj);
-    fs.writeFile(outputFile, dataString, 'utf8');
-  })
+  combinedCrimes.push(crimes);
 });
+
+let dataString = JSON.stringify(combinedCrimes);
+fs.writeFile(outputFile, dataString, 'utf8');
